7d0b5e804e65dbf02832821fee796cb897e71651
kenkono <twentynresult@yahoo.co.jp>
2020-11-23 10:29:08 +0900

    add

--- pos.py
+++ pos.py
@@ -2,23 +2,27 @@ import csv
 import datetime as dt
 import eel
 
+
 ### 商品クラス
 class Item:
+
     def __init__(self,item_code,item_name,price):
-        self.item_code=item_code
-        self.item_name=item_name
-        self.price=price
+        self.item_code = item_code
+        self.item_name = item_name
+        self.price = price
     
     def get_price(self):
         return self.price
 
+
 ### オーダークラス
 class Order:
+
     def __init__(self,item_master):
-        self.item_order_list=[]
-        self.item_list=[]
-        self.item_number_list=[]
-        self.item_master=item_master
+        self.item_order_list = []
+        self.item_list = []
+        self.item_number_list = []
+        self.item_master = item_master
     
     def add_item_order(self,item_code):
         self.item_order_list.append(item_code)
@@ -31,16 +35,6 @@ class Order:
     def view_item_list(self):
         for item in self.item_order_list:
             eel.view_item_code(item)
-
-    # 個数の表示
-    # def view_item_number(self):
-    #     for item in self.item_number_list:
-            # eel.view_total_price("注文商品個数:{}".format(item))
-    
-    # マスター情報の表示
-    # def view_master_list(self):
-    #     for master in self.item_master:
-    #         print("マスター情報 ID:{} 商品名:{} 価格:{}".format(master.item_code,master.item_name,master.price))
 
     # オーダー商品の表示
     def view_order_list(self):
@@ -57,25 +51,27 @@ class Order:
         for order_item in self.item_order_list:
             for master in self.item_master:
                 if master.item_code == order_item:
-                    sum_price+=int(master.price)
+                    sum_price += int(master.price)
         return sum_price
 
     # オーダー商品の合計個数
     def sum_order_quantity(self):
-        sum_quantity=0
+        sum_quantity = 0
         for number in self.item_number_list:
-            sum_quantity+=int(number)
+            sum_quantity += int(number)
         eel.view_order_quantity(sum_quantity)
         return sum_quantity
 
+
 class Master:
+
     def __init__(self):
         pass
 
     def read_master_item(self, csv_file):
         with open(csv_file) as f:
-            item_master=[]
-            reader=csv.reader(f)
+            item_master = []
+            reader = csv.reader(f)
             for row in reader:
                 item_master.append(Item("{}".format(row[0]),"{}".format(row[1]),"{}".format(row[2])))
             return item_master
@@ -92,13 +88,14 @@ class Master:
         with open('txt/{}_receipt.txt'.format(time), mode='w') as f:
             f.write(receipt_text)
 
+
 ### メイン処理
 def main(item_code, order_quantity):
-    master=Master()
-    item_master=master.read_master_item('master.csv')
+    master = Master()
+    item_master = master.read_master_item('master.csv')
     
     # オーダー商品コード登録
-    order=Order(item_master)
+    order = Order(item_master)
     order.add_item_order(item_code)
     
     # オーダー個数入力
@@ -108,24 +105,17 @@ def main(item_code, order_quantity):
     order.view_item_list()
 
     # オーダーの合計個数表示
-    sum_order_quantity=order.sum_order_quantity()
+    sum_order_quantity = order.sum_order_quantity()
 
     # オーダーの合計金額表示
-    sum_order_price=sum_order_quantity*order.sum_order_price()
+    sum_order_price = sum_order_quantity*order.sum_order_price()
     eel.view_total_price(sum_order_price)
-
-
-    # お釣りの表示
-    # change=int(master.pay_amount(pay_amount))-(sum_order_price)
-    # eel.view_change("お釣り:{}".format(change))
 
     # テキスト出力
     # receipt_text = "注文商品: " + str(order.view_order_list()[0].item_name)+ "\n" +"単価: " + str(order.view_order_list()[0].price)+ "\n" +"合計金額: " + str(sum_order_price)+ "\n" + "合計個数: " + str(sum_order_quantity)+ "\n" + "お釣り: " + str(change)
     # master.output_txt(receipt_text)
 
+
 def show_change(pay_amount, total_price):
-    # master=Master()
-    print(pay_amount)
-    print(total_price)
-    change=(pay_amount)-(total_price)
+    change = int(pay_amount)-int(total_price)
     eel.view_change(change)
--- view.py
+++ view.py
@@ -2,9 +2,11 @@ import eel
 import desktop
 import pos
 
+
 app_name="html"
 end_point="index.html"
 size=(700,600)
+
 
 # @ eel.expose
 # def kimetsu_search(word,csv_name):
@@ -13,9 +15,9 @@ size=(700,600)
 def main_1(item_code, order_quantity):
     pos.main(item_code, order_quantity)
 
+
+@ eel.expose
 def main_2(pay_amount, total_price):
-    print(pay_amount)
-    print(total_price)
     pos.show_change(pay_amount, total_price)
 desktop.start(app_name,end_point,size)
 #desktop.start(size=size,appName=app_name,endPoint=end_point)
